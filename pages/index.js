5f4802f2669c5c75ef5725ddbb1589c08ab7c798
chrisachinga <achinga.chris@gmail.com>
2022-01-15 14:06:31 +0300

    getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Layout from "../layouts/Layout"
+import Layout from '../layouts/Layout'
 
-const index = () => {
-  return (
-    <Layout>
-      Ahaa
-    </Layout>
-  )
+const index = ({aircraft}) => {
+  return <Layout>Ahaa
+
+    <div>
+      <h1>Aircraft Name: { aircraft}</h1>
+    </div>
+  </Layout>
 }
 
 export default index
+
+export function getStaticProps() {
+  return {
+    props: { aircraft: 'Airbus A350-941' },
+  }
+}
